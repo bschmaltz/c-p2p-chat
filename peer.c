19fcd67e648be84ee83a36402c6cf5486f16cff7
Yiqi (Victor) Chen <yiqic1993@gmail.com>
2014-07-21 16:26:43 -0400

    parse_arg

--- peer.c
+++ peer.c
@@ -13,15 +13,23 @@
 #include <locale.h>
 
 // Globals
+int tracker_port;
+char tracker_ip[20];
+
 
 // Function Prototypes
 void parse_args(int argc, char **argv);
 
 int main(int argc, char **argv){
-  parse_args(argc, argv);
-  return 0;
+parse_args(argc, argv);
+	
+	return 0;
 }
 
 void parse_args(int argc, char **argv){
-
+	if (argc != 3) {
+		fprintf(stderr, "%s\n", "Argument number not correct");
+	}
+	tracker_port = atoi(argv[2]);
+	memcpy(tracker_ip, argv[1], (strlen(argv[1]) + 1 > sizeof(tracker_ip)) ? sizeof(tracker_ip) : strlen(argv[1]));
 }
